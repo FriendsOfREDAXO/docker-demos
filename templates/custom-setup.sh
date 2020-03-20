631639d4cfc2ddfb10e0038f9eeb35fdeb374fb8
schuer <ds@decaf.de>
2020-03-20 12:08:27 +0100

    add setup flow (accidentally deleted before)

--- templates/custom-setup.sh
+++ templates/custom-setup.sh
@@ -3,6 +3,11 @@ set -euo pipefail
 
 cd /var/www/html
 
+php redaxo/bin/console install:download -q %%PACKAGE%% %%VERSION%%
+php redaxo/bin/console package:install -q %%PACKAGE%%
+php redaxo/bin/console cache:clear -q
+php redaxo/bin/console %%PACKAGE%%:install -q -y
+
 # workaround until R5.10.1 is released.
 mysql -h$REDAXO_DB_HOST \
       -u$REDAXO_DB_LOGIN \
