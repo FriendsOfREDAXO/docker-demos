d900f5b5e00051220cdc9f463c82bf5c060c3118
schuer <ds@decaf.de>
2020-03-20 12:01:29 +0100

    build

---
+++ community/custom-setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -euo pipefail
+
+cd /var/www/html
+
+# workaround until R5.10.1 is released.
+mysql -h$REDAXO_DB_HOST \
+      -u$REDAXO_DB_LOGIN \
+      -p$REDAXO_DB_PASSWORD \
+      -D$REDAXO_DB_NAME \
+      -e "UPDATE rex_user SET login_tries = 0;" \
+      2> /dev/null
+
+chown -R www-data:www-data ./
