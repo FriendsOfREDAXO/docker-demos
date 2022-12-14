76652466c69e82f4c2f39f94f8c1dd705c352a90
Dirk Schürjohann <ds@decaf.de>
2022-12-14 17:44:55 +0100

    bump demo addons to latest versions (#9)

--- community/custom-setup.sh
+++ community/custom-setup.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 cd /var/www/html
 
-php redaxo/bin/console install:download -q demo_community 3.3.1
+php redaxo/bin/console install:download -q demo_community 4.0.0
 php redaxo/bin/console package:install -q demo_community
 php redaxo/bin/console cache:clear -q
 php redaxo/bin/console demo_community:install -q -y
