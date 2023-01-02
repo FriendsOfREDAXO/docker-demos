99ac3a42e90ee1a8eaaa1ef44f694cb316e0f08b
schuer <ds@decaf.de>
2023-01-02 16:22:48 +0100

    bump demo addons to latest versions

--- update.sh
+++ update.sh
@@ -22,8 +22,8 @@ variants=( base community onepage )
 # declare package versions
 declare -A packageVersions=(
     [base]='3.0.1'
-    [community]='4.0.1'
-    [onepage]='1.7.0'
+    [community]='4.0.2'
+    [onepage]='1.7.1'
 )
 
 # -----------------------------------------------------------------------------
