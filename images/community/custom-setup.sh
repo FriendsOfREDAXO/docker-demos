493bfd423e7b36df6efc29a1e6d3af898276b7e6
FriendsOfREDAXO-T <FriendsOfREDAXO-T@users.noreply.github.com>
2023-12-28 22:33:50 +0000

    GenerRRRRR-ate image files from source!

--- images/community/custom-setup.sh
+++ images/community/custom-setup.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 cd /var/www/html
 
-php redaxo/bin/console install:download -q demo_community 4.0.2
+php redaxo/bin/console install:download -q demo_community 4.1.2
 php redaxo/bin/console package:install -q demo_community
 php redaxo/bin/console cache:clear -q
 php redaxo/bin/console demo_community:install -q -y
